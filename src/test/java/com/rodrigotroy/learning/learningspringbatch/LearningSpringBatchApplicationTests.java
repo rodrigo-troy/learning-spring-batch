654ba4fc17b218ed0ac46b43a737e7c63a88690b
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-04-03 21:40:48 -0400

    Initial commit

---
+++ src/test/java/com/rodrigotroy/learning/learningspringbatch/LearningSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigotroy.learning.learningspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
